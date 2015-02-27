e0c7dc815abd60ba1835444ed9beaf7d332afb53
taigrus <raulburgos007@gmail.com>
2015-02-27 18:17:55 -0400

    comentario borrado
    borre unos comentarios

--- ProyectoPruebaNetBeansGitHub/src/proyectopruebanetbeansgithub/ProyectoPruebaNetBeansGitHub.java
+++ ProyectoPruebaNetBeansGitHub/src/proyectopruebanetbeansgithub/ProyectoPruebaNetBeansGitHub.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package proyectopruebanetbeansgithub;
 
-/**
- *
- * @author rburgos
- */
 public class ProyectoPruebaNetBeansGitHub {
 
     /**
@@ -16,7 +7,6 @@ public class ProyectoPruebaNetBeansGitHub {
      */
     public static void main(String[] args) {
         System.out.println("Este es un programa desde netbeans para probar git");
-        // TODO code application logic here
     }
     
 }
